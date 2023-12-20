47af4cd740c0e5355bab2cfb27bcc7415ccef46e
Arbab666 <141415239+Arbab666@users.noreply.github.com>
2023-12-20 17:31:07 +0500

    Add files via upload

---
+++ database/migrations/2022_05_13_220658_create_affiliates_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('affiliates', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->foreignId('merchant_id');
+            // TODO: Replace me with a brief explanation of why floats aren't the correct data type, and replace with the correct data type.
+            // I replaced the float data type for commission_rate with decimal. Using decimal is generally recommended for representing fixed-point numbers (e.g., currency) to avoid precision issues that can occur with floating-point numbers
+            $table->decimal('commission_rate', 8, 2);
+            $table->string('discount_code');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('affiliates');
+    }
+};
